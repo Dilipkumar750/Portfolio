7212f224045255f9bb15aa28ba40c1d7f9167c5f
Dilipkumar750 <dilipk5406@gmail.com>
2025-03-05 11:34:56 +0530

    changes

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -40,7 +40,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "/resume.pdf",
+      href: "/dilip.pdf",
       style: "rounded-br-md",
       download: true,
     },
